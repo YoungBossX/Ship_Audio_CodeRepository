cb619e26b480a34e9795f93c2e5c439af3ce2495
Xie Caicheng <87680698+YoungBossX@users.noreply.github.com>
2025-04-30 13:28:18 +0800

    commit

---
+++ models/CLIP/clip.py
@@ -0,0 +1,5 @@
+# -*-coding: utf-8 -*-
+# @Time    : 2025/4/29 11:20
+# @Author  : XCC
+# @File    : clip.py
+# @Software: PyCharm
---
+++ models/CLIP/img_encoder.py
@@ -0,0 +1,5 @@
+# -*-coding: utf-8 -*-
+# @Time    : 2025/4/29 11:19
+# @Author  : XCC
+# @File    : img_encoder.py
+# @Software: PyCharm
